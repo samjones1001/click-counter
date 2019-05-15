55b41986153e5986adabe923db85d0a37d5ed3f4
Sam Jones <sam.jones494@googlemail.com>
2019-05-15 21:53:30 +0100

    initial setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapeter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new EnzymeAdapeter() })
